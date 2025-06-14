d2c74a67c57e79c6efbc63bc976acaa819ad7130
elsakupfer <elsa.kupfer.1@ens.etsmtl.ca>
2025-06-14 12:32:09 -0400

    * creation fonction charger waypoints à partir base de donnée

---
+++ main.elsa.py
@@ -0,0 +1,21 @@
+
+
+
+#
+def charger_waypoints(fichier_csv):
+    waypoints = []
+    with open(fichier_csv, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # sauter l'en-tête
+        for ligne in reader:
+            if len(ligne) < 3:
+                continue
+            try:
+                id_wp = ligne[0]
+                lat = float(ligne[1])
+                lon = float(ligne[2])
+                waypoints.append((id_wp, lat, lon))
+            except ValueError:
+                continue
+    return waypoints
+# Renvoie une liste de tuple ('ID', (latitude, longitude))
