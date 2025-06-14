a5721e6794f8e46a67bcf3ec3d15b19fe1fc7cc2
villamandos <enzzovillamandos@gmail.com>
2025-06-14 17:07:57 -0400

    donnees meteo prelever sur une api

--- creation_csv.py
+++ creation_csv.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-df = pd.read_csv("Data/Waypoint.csv")
+"""df = pd.read_csv("Data/Waypoint.csv")
 north_america_codes = ['US', 'CA', 'MX']
 df_na = df[df['iso_country'].isin(north_america_codes)]
 waypoints = df_na[['ident', 'latitude_deg', 'longitude_deg', 'iso_country']]
 print(waypoints.head())
-waypoints.to_csv("Data/waypoints.csv", index=False)
+waypoints.to_csv("Data/waypoints.csv", index=False)"""
 
 
 
